c46c079610ce8be155a35f39c69cc81412f46c6e
Chase Frankenfeld <chasefrankenfeld@gmail.com>
2017-02-19 20:06:55 +1100

    Add rpec and authorisation to delete articles

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -2,7 +2,44 @@ require 'rails_helper'
 
 RSpec.describe "Articles", type: :request do
   before do
-    @article = Article.create(title: "Title One", body: "Body of article one")
+    @john = User.create(email: "john@example.com", password: "password")
+    @fred = User.create(email: "fred@example.com", password: "fredpassword")
+    @article = Article.create!(title: "Title One", body: "Body of article one", user: @john)
+  end
+
+  describe "GET /articles/:id/edit" do
+    context "with non-signed user" do
+      before { get "/articles/#{@article.id}/edit" }
+
+      it "redirects to the signin page" do
+        expect(response.status).to eq(302)
+        flash_message = "You need to sign in or sign up before continuing."
+        expect(flash[:alert]).to eq(flash_message)
+      end
+
+    end
+    context "with signed user who is a non-owner of an article" do
+      before do
+        login_as(@fred)
+        get "/articles/#{@article.id}/edit"
+      end
+
+      it "redirects to the home page" do
+        expect(response.status).to eq(302)
+        flash_message = "You can only edit you own article."
+        expect(flash[:alert]).to eq(flash_message)
+      end
+    end
+
+    context "with signed user as owner successful edit" do
+      before do
+        login_as(@john)
+        get "/articles/#{@article.id}/edit"
+      end
+      it "successfully edits article" do
+        expect(response.status).to eq(200)
+      end
+    end
   end
 
   describe 'GET /articles/:id' do
@@ -23,7 +60,41 @@ RSpec.describe "Articles", type: :request do
         expect(flash[:alert]).to eq(flash_message)
       end
     end
+  end
 
+  describe 'DELETE /articles/:id' do
+    context "with non-signed user" do
+      before { delete "/articles/#{@article.id}" }
+
+      it "redirects to the signin page" do
+        expect(response.status).to eq(302)
+        flash_message = "You need to sign in or sign up before continuing."
+        expect(flash[:alert]).to eq(flash_message)
+      end
+    end
+    context "with signed user who is a non-owner of an article" do
+      before do
+        login_as(@fred)
+        delete "/articles/#{@article.id}"
+      end
+
+      it "redirects to the home page" do
+        expect(response.status).to eq(302)
+        flash_message = "You can only delete your own article."
+        expect(flash[:danger]).to eq(flash_message)
+      end
+    end
+    context "with signed user as owner successful delete" do
+      before do
+        login_as(@john)
+        delete "/articles/#{@article.id}"
+      end
+      it "successfully deletes article" do
+        expect(response.status).to eq(302)
+        flash_message = "Article has been deleted."
+        expect(flash[:success]).to eq(flash_message)
+      end
+    end
   end
 
 end
