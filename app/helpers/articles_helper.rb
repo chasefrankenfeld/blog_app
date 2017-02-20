f07a5cc5dfa8cb66820461aa17a2ca3ed36054ad
Chase Frankenfeld <chasefrankenfeld@gmail.com>
2017-02-20 15:35:25 +1100

    Force sign in before positng comments

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,7 +3,9 @@ module ArticlesHelper
   # We use these helper methods to had functions that we are going to use in our views
 
   def persisted_comments(comments)
-    comments.reject{ |comment| comment.new_record? }  # means we only want comments that are coming from the database. Nothing that is new.
+    comments.reject{ |comment|  # reject - returns an array for all elements of enum for which block is false
+                      comment.new_record?  # .new_record? - returns true if this object hasn’t been saved yet
+                    }  # means we only want comments that are coming from the database - nothing that is new and hasn't been saved yet
   end
 
 end
